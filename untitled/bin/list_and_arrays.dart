83763834de58932955926f9e1e508adf5ce29474
Mahmoud_Madih <moodbeder150@gmail.com>
2024-07-02 15:52:47 +0300

    Lisrt , Set , Map

---
+++ untitled/bin/list_and_arrays.dart
@@ -0,0 +1,46 @@
+import 'package:test/expect.dart';
+
+void main (){
+// var My_frinds_list =["ahmed", "omer","mahmoud",1];
+//   My_frinds_list.add('mohmed');
+//   My_frinds_list.add(15);
+//   My_frinds_list.reversed;
+//   if(My_frinds_list.isEmpty){
+//     print ('empty');{
+//   try{
+//     print(My_frinds_list[3]);
+//   }catch(e){
+//     print ("handel exception ");
+//
+//   }}}
+// List<int> hach=[1,2,6,8];
+//   var My_frinds_list =["ahmed", "omer","mahmoud",1];
+//   print(My_frinds_list.contains("mahmoud"));+
+/*
+List Vs Set va map
+ */
+// set unordered data
+// Set<dynamic> y={1,2,3,5,6,89};
+// print(y.length);
+// y.add("hfgj");
+// print (y);
+/// map data structure
+  /// use key and value
+
+    Map k = {
+      "name": "mahmoud",
+      "age": 15,
+      "major": "engineering",
+      0: "sdjdd"
+    };
+
+    k.addAll({"address": "alexandria"});
+
+    print(k);
+  }
+
+
+
+
+
+
