10ed0ddda59f9365e564672d8e41de5c36fb3899
Mahmoud_Madih <moodbeder150@gmail.com>
2024-07-08 16:29:20 +0300

    compsition

---
+++ untitled/bin/oop_session/cpmpsition/engine.dart
@@ -0,0 +1,18 @@
+class Engine{
+  late String type;
+  late String power;
+
+
+
+  String get Power => power;
+
+  set Power(String value) {
+    power = value;
+  }
+
+  String get Type => type;
+
+  set Type(String value) {
+    type = value;
+  }
+}
